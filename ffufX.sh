999fd65023e0ac1e4a24255a958f28467d406891
asilentwolf <forwebsec@gmail.com>
2024-05-21 09:11:14 -0400

    Changes

---
+++ ffufX.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+exec 2>/dev/null
+
+D="$1"
+fX=$(mktemp --suffix=".txt")
+
+if [ -t 0 ]; then
+    :
+else
+  cat > $fX
+fi
+
+if [ ! -s "$fX" ]; then
+    cat $D > $fX
+else
+    :
+fi
+
+NJobs=20 ; export NJobs
+Jobs(){
+	if [[  $(jobs | wc -l ) -ge $NJobs ]] ; then
+		wait -n
+	fi
+}
+
+function main(){
+    x=$1
+    rX=$(mktemp --suffix=".txt")
+    # Fetch data using ffuf and append to temporary file
+    ffuf -w db/Allmiro.txt -u "$x/FUZZ" -mc 200 -fs 0 -sf -se -recursion | awk '{print $1}' | sed 's/[^[:print:]]//g' | sed 's/\[2K//g' > "$rX"
+    for i in $(cat "$rX"); do
+        echo "$x/$i" 
+    done
+}
+
+for d in $(cat $fX); do
+    main $d
+done
