431a6d7f50bb436e9d3f6c2b9c7455579fd96aaa
asilentwolf <forwebsec@gmail.com>
2024-05-21 22:08:50 -0400

    Changes

--- ffufX.sh
+++ ffufX.sh
@@ -27,7 +27,7 @@ function main(){
     x=$1
     rX=$(mktemp --suffix=".txt")
     # Fetch data using ffuf and append to temporary file
-    ffuf -w db/API.txt -u "$x/FUZZ" -mc 200 -fs 0 -sf -se -recursion | awk '{print $1}' | sed 's/[^[:print:]]//g' | sed 's/\[2K//g' > "$rX"
+    ffuf -w db/API.txt -u "$x/FUZZ" -mc 200 -fs 0 -sf -se | awk '{print $1}' | sed 's/[^[:print:]]//g' | sed 's/\[2K//g' > "$rX"
     for i in $(cat "$rX"); do
         echo "$x/$i"
     done | sort -u | python3 utils/Uniqe.py
